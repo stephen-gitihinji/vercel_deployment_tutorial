3c4a65810a83c5b7f9314979e7d663126d5192b2
stephen-githinji <kiamastephen559@gmail.com>
2026-07-15 15:36:08 +0300

    frontend to render

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="framework")
 
 @app.get("/")
 def index():
@@ -10,7 +14,9 @@ def index():
         {"name":"John", "age":21}
     ]
 
-
+@app.get("/home", response_class=HTMLResponse)
+def home_page(request: Request):
+    return templates.TemplateResponse(name="index.html", request=request, context={"message": "My Profile"})
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
